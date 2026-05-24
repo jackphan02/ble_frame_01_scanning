3de46dd4b211f22f0eb1ab82181c0ddd1bef394c
Jack Phan <jackphan02@gmail.com>
2026-05-24 09:50:20 -0700

    add ble 01

---
+++ BleFrame/Data/UserBlePeripheral.swift
@@ -0,0 +1,29 @@
+//
+//  UserBlePeripheral.swift
+//  BleFrame
+//
+//  Created by Jack Phan on 6/11/23.
+//
+
+import CoreBluetooth
+
+class UserBlePeripheral: Identifiable, ObservableObject {
+    
+    @Published var id: UUID
+    @Published var userPeripheral: CBPeripheral
+    @Published var name: String
+    @Published var rssi: Int
+    
+    init(_userPeripheral: CBPeripheral,
+         _name: String,
+         _rssi: Int
+         )
+    {
+        id = UUID()
+        userPeripheral = _userPeripheral
+        name = _name
+        rssi = _rssi
+
+    }
+    
+}
